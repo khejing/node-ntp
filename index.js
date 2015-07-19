d340bb87d1f796df9888c9607ca06a2e64c90185
khejing <khejing3@aliyun.com>
2015-07-19 14:00:58 +0800

    编码完成

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by maya on 2015/7/19.
+ */
+var mqtt = require('mqtt');
+
+var timeSyncTopic = "timesync";
+var mqttClientInstance = mqtt.connect("mqtt://localhost:1883", {clientId: timeSyncTopic});
+mqttClientInstance.on('connect', function () {
+    console.log("connect mqtt server success");
+});
+mqttClientInstance.on('error',function(error) {
+    console.log("mqtt connect failed: ", error);
+});
+mqttClientInstance.subscribe(timeSyncTopic);
+
+mqttClientInstance.on('message', function(messageTopic, data) {
+    var msg = JSON.parse(data);
+    if(msg.timesync === "Request"){
+        mqttClientInstance.publish(msg.clientId, JSON.stringify({timesync: "Response", serverTime: Date.now()}));
+    }
+});
+/* when got exit signal, then exit
+ mqttClientInstance.end();
+ mqttClientInstance = null;
+ console.log("mqtt client has been destroyed");
+ */
